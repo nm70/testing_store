3c1623b4a6d80f93854fa324a56c02bfbb8711df
Nataly Maksimova <sahaling70@gmail.com>
2021-12-24 19:28:28 +0500

    методы проверки PageObject

--- pages/main_page.py
+++ pages/main_page.py
@@ -2,9 +2,11 @@ from .base_page import BasePage
 from selenium.webdriver.common.by import By
 
 class MainPage(BasePage):
-
+    # перейдите на страницу входа
     def go_to_login_page(self):
         login_link = self.browser.find_element(By.ID, "login_link")
         login_link.click()
 
-
+    # должна быть ссылка для входа
+    def should_be_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
